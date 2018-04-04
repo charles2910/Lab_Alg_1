eec39da8d55f05b6d2c88fbe09dfa0d2b4e2369d
Charles <chlm2910@gmail.com>
2018-04-04 16:01:07 -0300

    correção do algoritmo merge sort iterativo

--- merge_sort_iteration.c
+++ merge_sort_iteration.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+// A função recebe vetores crescentes v[p..q-1]
+// e v[q..r-1] e rearranja v[p..r-1] em ordem
+// crescente.
+
+static void
+intercala (int p, int q, int r, int v[])
+{
+   int i, j, k, *w;                        //  1
+   w = malloc ((r-p) * sizeof (int));     //  2
+   i = p; j = q;                           //  3
+   k = 0;                                  //  4
+
+   while (i < q && j < r) {                //  5
+      if (v[i] <= v[j])  w[k++] = v[i++];  //  6
+      else  w[k++] = v[j++];               //  7
+   }                                       //  8
+   while (i < q)  w[k++] = v[i++];         //  9
+   while (j < r)  w[k++] = v[j++];         // 10
+   for (i = p; i < r; ++i)  v[i] = w[i-p]; // 11
+   free (w);                               // 12
+}
+
 
 // Esta função rearranja o vetor v[0..n-1]
 // em ordem crescente.
@@ -37,7 +59,7 @@ int main ()
         for (int i = 0; i < tam; i++)  //atribui valores aleatorios
             vetor[i] = rand() % tam; //ao vetor de entrada
         start = clock();
-        mergesort_i(tam, vetor);
+        mergesort_i( tam, vetor);
         end = clock();
         tempo = ((double) end - start) / CLOCKS_PER_SEC;
         printf ("%f\n", tempo);
